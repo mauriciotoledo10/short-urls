330490cbcc3f36773937e432d761a0ccab7bbc69
Mauricio Toledo <mauricionrk@gmail.com>
2018-09-23 20:46:43 -0300

    mais alguns métodos adicionados

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -11,14 +11,15 @@ class User_model extends CI_Model{
         $data['passw'] = password_hash($data['passw'], PASSWORD_DEFAULT);
         $this->db->insert('users',$data);
         $userID = $this->db->insert_id();
-        
+
         if($userID){
             return $this->GetUser($userID);
         }else{
             return false;
     }
 }
-
+    
+    //método responsável por recuperar dados do usuário cujo id foi passado como parâmetro
     public function GetUser($id){
         $this->db->select('*')->from('users')->where('id',$id);
         $result = $this->db->get()->result();
@@ -28,5 +29,21 @@ class User_model extends CI_Model{
             return false;
         }
     }
+
+    //método responsável por atualizar os dados de um usuário no banco.
+    function Update($data){
+        $data['passw'] = password_hash($data['passw'], PASSWORD_DEFAULT);
+        $this->db->where('id', $this->session->userdata('id'));
+        $this->db->update('users', $data);
+    }
+
+    //método que vai retornar os dados de um usuário que está tentando fazer login.
+    function Login($data){
+        $this->db->select('*')->from('users')->where('email',$data['email']);
+        $results = $this->db->get()->result();
+        return $results;
+    }
+
+
 
 }
