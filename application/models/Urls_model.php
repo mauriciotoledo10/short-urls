90133fe4e83e22a83be4acb111bc0f15a90cd3e8
Mauricio Toledo <mauricionrk@gmail.com>
2018-09-23 19:43:58 -0300

    definindo método construtor para o model

--- application/models/Urls_model.php
+++ application/models/Urls_model.php
@@ -1,5 +1,10 @@
 <?php
 if(!defined('BASEPATH')) exit('No direct script access allowed');
 class Urls_model extends CI_Model{
+    
+    //definindo nosso método construtor
+    function __construct(){
+        parent::__construct();
+    }
 
 }
