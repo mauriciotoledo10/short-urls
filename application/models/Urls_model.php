27b1e3f4859cc5ddebd8fb8b787baa20ee3f7230
Mauricio Toledo <mauricionrk@gmail.com>
2018-09-23 19:50:37 -0300

    método que gera código único da url original

--- application/models/Urls_model.php
+++ application/models/Urls_model.php
@@ -7,4 +7,16 @@ class Urls_model extends CI_Model{
         parent::__construct();
     }
 
+    //método que vai gerar o código único que representará a URL original.
+    //é usado com um do while (condição) p/ verificar se o código alfanumérico de 8 dígitos gerado já foi
+    //utilizado ou se está disponível. Enquanto o código gerado não for um código único, não haverá retorno....
+    //essa é a função do (do while (condição))
+    function GenerateUniqueCode(){
+        do{
+        $code = random_string('alnum',8);
+        $this->db->from('urls')->where('code',$code);
+        $num = $this->db->count_all_results();
+        }while($num >= 1);
+        return $code;
+    }
 }
