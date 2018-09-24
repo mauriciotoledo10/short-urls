ed56b952c5034da25604be6f9acb1a157aca6ce9
Mauricio Toledo <mauricionrk@gmail.com>
2018-09-24 11:59:09 -0300

    footer

---
+++ application/views/commons/foot.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+<div class="container">
+<p class="text-muted">Encurtador de URLs <strong>Mauricio Toledo</strong></p>
+</div>
+</footer>
--- application/views/commons/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="footer">
-<div class="container">
-<p class="text-muted">Exemplo do capítulo 12 do livro <strong>
-CodeIgniter Teoria na Prática</strong></p>
-</div>
-</footer>
